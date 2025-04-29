93a116983b911e51252d5e66673ae5bb0aec66c6
PankovAS <pankoff.aleksey2004@yandex.ru>
2025-04-29 17:30:50 +0500

    Добавил музыку как сущность в бд

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Users (
     Id INTEGER PRIMARY KEY AUTOINCREMENT,
     Full_name TEXT NOT NULL,
     Login TEXT NOT NULL UNIQUE,
@@ -9,13 +9,13 @@ CREATE TABLE Users (
     FOREIGN KEY (ResearcherId) REFERENCES Users(Id)
 );
 
-CREATE TABLE Researcher_Details (
+CREATE TABLE IF NOT EXISTS Researcher_Details (
     UserId INTEGER PRIMARY KEY,
     Number_of_patients INTEGER NOT NULL,
     FOREIGN KEY (UserId) REFERENCES Users(Id)
 );
 
-CREATE TABLE Investigated_Details (
+CREATE TABLE IF NOT EXISTS Investigated_Details (
     UserId INTEGER PRIMARY KEY,
     Occupation TEXT NOT NULL,
     EfficiencyWithMusic REAL,
@@ -23,19 +23,27 @@ CREATE TABLE Investigated_Details (
     FOREIGN KEY (UserId) REFERENCES Users(Id)
 );
 
-CREATE TABLE Examples (
+CREATE TABLE IF NOT EXISTS Examples (
     Id INTEGER PRIMARY KEY AUTOINCREMENT,
     Expression TEXT NOT NULL,
     CorrectAnswer INTEGER NOT NULL
 );
 
-CREATE TABLE Sessions (
+CREATE TABLE IF NOT EXISTS Sessions (
     Id INTEGER PRIMARY KEY AUTOINCREMENT,
     UserId INTEGER NOT NULL,
     SessionDate TEXT NOT NULL,
     CorrectAnswers INTEGER NOT NULL,
     ElapsedSeconds INTEGER NOT NULL,
     Efficiency REAL NOT NULL,
-    WithMusic INTEGER NOT NULL DEFAULT 0,  -- 0: без музыки, 1: с музыкой (для будущих тестов с музыкой)
+    WithMusic INTEGER NOT NULL DEFAULT 0,  -- 0: без музыки, 1: с музыкой 
     FOREIGN KEY (UserId) REFERENCES Users(Id)
+);
+
+CREATE TABLE IF NOT EXISTS Music (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Author TEXT NOT NULL,
+    Title TEXT NOT NULL,
+    Duration INTEGER NOT NULL,  
+    FilePath TEXT NOT NULL     
 );
---
+++ database/music_seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO Music (Author, Title, Duration, FilePath)
+VALUES ('Tunetank', 'Gameboy', 120, 'database/music/Tunetank - Gameboy.mp3');
