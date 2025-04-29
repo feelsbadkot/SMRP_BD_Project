0a9ea9ed0dfdeddff85395ac9c7a88cff49cb252
Dima Drachev <dima.drachev.2004@mail.ru>
2025-04-29 18:55:35 +0500

    Загрузил треки в базу данных

--- database/music_seed.sql
+++ database/music_seed.sql
@@ -1,2 +1,19 @@
-INSERT INTO Music (Author, Title, Duration, FilePath)
-VALUES ('Tunetank', 'Gameboy', 120, 'database/music/Tunetank - Gameboy.mp3');
+DELETE FROM Music;
+INSERT INTO Music (Author, Title, Duration, FilePath) VALUES 
+('Tunetank', 'Gameboy', 120, 'database/music/Tunetank - Gameboy.mp3'),
+('Tunetank', 'Beauty Beat', 156, 'database/music/Tunetank - Beauty Beat.mp3'),
+('Tunetank', 'Felt the Same', 144, 'database/music/Tunetank - Felt the Same.mp3'),
+('Tunetank', 'Holiday Road', 109, 'database/music/Tunetank - Holiday Road.mp3'),
+('Tunetank', 'Lazy Bones', 93, 'database/music/Tunetank - Lazy Bones.mp3'),
+('Tunetank', 'Levitation', 80, 'database/music/Tunetank - Levitation.mp3'),
+('Tunetank', 'Locked in the Room', 126, 'database/music/Tunetank - Locked in the Room.mp3'),
+('Tunetank', 'Lost in Thoughts', 177, 'database/music/Tunetank - Lost in Thoughts.mp3'),
+('Tunetank', 'Lovely Day', 118, 'database/music/Tunetank - Lovely Day.mp3'),
+('Tunetank', 'Night Moon', 122, 'database/music/Tunetank - Night Moon.mp3'),
+('Tunetank', 'Gameboy', 131, 'database/music/Tunetank - Night Shift.mp3'),
+('Tunetank', 'Gameboy', 131, 'database/music/Tunetank - Night Shift.mp3'),
+('Tunetank', 'Pleasant Evening', 100, 'database/music/Tunetank - Pleasant Evening.mp3'),
+('Tunetank', 'Retro Groove', 182, 'database/music/Tunetank - Retro Groove.mp3'),
+('Tunetank', 'Stay Lo-Fi', 101, 'database/music/Tunetank - Stay Lo-Fi.mp3'),
+('Tunetank', 'Urban', 91, 'database/music/Tunetank - Urban.mp3'),
+('Tunetank', 'Western Lo-Fi', 122, 'database/music/Tunetank - Western Lo-Fi.mp3');
